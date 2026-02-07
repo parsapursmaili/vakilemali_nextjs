1eb0f02d55eb0bdc6c45a0313c9fcef18778710e
parsapursmaili <parsapursmaili@gmail.com>
2026-02-07 20:22:22 +0330

    حل مشکل robots.txt

--- app/robots.js
+++ app/robots.js
@@ -1,22 +1,14 @@
 // app/robots.js
 
 export default function robots() {
-  // آدرس اصلی سایت خود را اینجا جایگزین کنید
-  const baseUrl = "https://vakilemali.com";
-
   return {
     rules: [
       {
         userAgent: "*",
-        allow: ["/"],
-        disallow: [
-          "/admin/", // صفحات مدیریت
-          "/login", // صفحه ورود
-          "/*?*", // جلوگیری از ایندکس شدن لینک‌های تکراری دارای پارامتر (سئو عالی)
-        ],
+        allow: "/",
+        disallow: ["/admin/", "/login", "/*?*"],
       },
     ],
-    // معرفی خودکار نقشه سایت
-    sitemap: `${baseUrl}/sitemap.xml`,
+    sitemap: "https://vakilemali.com/sitemap.xml",
   };
 }
